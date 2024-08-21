2c3d2e90ace7e67ce420e6d9a9da4d1ad7b7107c
Cob <25969162+TheCob11@users.noreply.github.com>
2024-08-21 10:08:25 -0400

    proved a bunch of theorems abt finitely-generated groups

--- PaulinIntroToAbstractAlgebra/Chapter3.lean
+++ PaulinIntroToAbstractAlgebra/Chapter3.lean
@@ -35,12 +35,108 @@ theorem lagranges_theorem [Group G] (S: Subgroup G) : card S ∣ card G :=
   ⟨k, hk⟩
 
 -- example corollary listed below the thm
-example [Group G] (hG : Nat.Prime (card G)) (S: Subgroup G) : S = ⊥ ∨ S = ⊤ :=
-  have _G_Finite := Nat.finite_of_card_ne_zero hG.ne_zero -- necessary for eq_top_of_card_eq
-  have card_eq_1_or_p : card S = 1 ∨ card S = card G := (Nat.dvd_prime hG).mp (lagranges_theorem S)
+theorem prime_order_subgroups_bot_top [Group G] [hG : Fact (card G).Prime] {S: Subgroup G} :
+  S = ⊥ ∨ S = ⊤ :=
+  have _G_Finite := Nat.finite_of_card_ne_zero hG.out.ne_zero -- necessary for eq_top_of_card_eq
+  have card_eq_1_or_p : card S = 1 ∨ card S = card G := (Nat.dvd_prime hG.out).mp (lagranges_theorem S)
   Or.imp (Subgroup.eq_bot_of_card_eq S) (Subgroup.eq_top_of_card_eq S) card_eq_1_or_p
 
 end SubgroupsCosetsLagrange
+
+section FinitelyGeneratedGroups
+
+theorem prime_order_cyclic [Group G] [h: Fact (Nat.card G).Prime] : IsCyclic G :=
+  have _G_Finite : Finite G := Nat.finite_of_card_ne_zero h.out.ne_zero
+  have _G_Fintype : Fintype G := Fintype.ofFinite G
+  have _G_FintypeG_Nontrivial : Nontrivial G := Fintype.one_lt_card_iff_nontrivial.mp
+    (Fintype.card_eq_nat_card ▸ h.out.one_lt)
+  have ⟨x, hx⟩ := exists_ne (1: G)
+  let gp_x := Subgroup.zpowers x
+  have gp_x_not_bot: gp_x ≠ ⊥ := Subgroup.zpowers_ne_bot.mpr hx
+  have gp_x_top: gp_x = ⊤ :=
+    Or.resolve_left prime_order_subgroups_bot_top gp_x_not_bot
+  ⟨x, (Subgroup.eq_top_iff' gp_x).mp gp_x_top⟩
+
+def cyclic_abelian [Group G] [h: IsCyclic G] : CommGroup G := .mk <| fun a b ↦
+  have ⟨x, hx⟩ := h.exists_generator;
+  have ⟨r, hr⟩ := Subgroup.mem_zpowers_iff.mp (hx a)
+  have ⟨s, hs⟩ := Subgroup.mem_zpowers_iff.mp (hx b)
+  calc a * b = x^r * x^s := hs ▸ hr ▸ rfl
+       _ = x ^ (r + s) := (zpow_add _ _ _).symm
+       _ = x ^ (s + r) := congrArg _ (add_comm _ _)
+       _ = x ^ s * x ^ r := zpow_add _ _ _
+       _ = b * a := hr ▸ hs ▸ rfl
+
+noncomputable def cyclic_infinite_Z [Group G] [Infinite G] [hG: IsCyclic G] :
+  G ≃* Multiplicative ℤ :=
+  -- for some reason ∃ can't be unboxed like ⟨x, hx⟩ in def (not theorem)
+  let x := hG.exists_generator.choose;
+  have hx : (g: G) → g ∈ Subgroup.zpowers x := hG.exists_generator.choose_spec;
+  have order_x_0 : orderOf x = 0 := Infinite.orderOf_eq_zero_of_forall_mem_zpowers hx
+  have not_isOfFinOrder_x : ¬IsOfFinOrder x := orderOf_eq_zero_iff.mp order_x_0
+  have h_pow (g: G) : ∃(k: ℤ), x ^ k = g := Subgroup.mem_zpowers_iff.mp (hx g);
+  let φ (g: G) : Multiplicative ℤ := (h_pow g).choose;
+  have φ_pow {n: ℤ} : φ (x ^ n) = n :=
+    have h_spec := (h_pow (x^n)).choose_spec
+    injective_zpow_iff_not_isOfFinOrder.mpr not_isOfFinOrder_x h_spec
+  let φ_inv (n: ℤ) : G := x ^ n;
+  have left_inv : Function.LeftInverse φ_inv φ := fun g => (h_pow g).choose_spec
+  have right_inv : Function.RightInverse φ_inv φ := @φ_pow
+  let φ_equiv : G ≃ Multiplicative ℤ := ⟨φ, φ_inv, left_inv, right_inv⟩
+  have map_mul' (g₁ g₂ : G) : φ (g₁ * g₂) = φ g₁ * φ g₂ :=
+    have ⟨a, ha⟩ := h_pow g₁;
+    have ⟨b, hb⟩ := h_pow g₂;
+    calc φ (g₁ * g₂) = φ (x ^ a * x ^ b) := ha ▸ hb ▸ rfl
+         _ = φ (x ^ (a + b)) := congrArg φ (zpow_add _ _ _).symm
+         _ = a + b := φ_pow
+         _ = φ (x ^ a) * φ (x ^ b) := φ_pow.symm ▸ φ_pow.symm ▸ rfl
+         _ = φ g₁ * φ g₂ := ha ▸ hb ▸ rfl
+  MulEquiv.mk φ_equiv map_mul'
+
+noncomputable def cyclic_finite_ZMod [Group G] [Fintype G] [hG: IsCyclic G] :
+  G ≃* Multiplicative (ZMod (Fintype.card G)) :=
+  let m := Fintype.card G;
+  let ZM := ZMod m;
+  have card_eq : m = Fintype.card (Multiplicative ZM) :=
+    (Fintype.card_multiplicative ZM ▸ ZMod.card m).symm;
+  -- for some reason ∃ can't be unboxed like ⟨x, hx⟩ in def (not theorem)
+  let x := hG.exists_generator.choose;
+  have hx : (g: G) → g ∈ Subgroup.zpowers x := hG.exists_generator.choose_spec;
+  have order_x_eq_m : orderOf x = m := orderOf_eq_card_of_forall_mem_zpowers hx
+  have _m_NeZero : NeZero m := NeZero.of_pos (order_x_eq_m ▸ orderOf_pos x)
+  have h_pow (g: G) : ∃(k: ℤ), x ^ k = g := Subgroup.mem_zpowers_iff.mp (hx g);
+  let φ (g: G) : Multiplicative ZM := ((h_pow g).choose: ZM);
+  let φ_inv (n: ZM) := x ^ (n.val: ℤ)
+  have zpow_zmpow_eq (a: ℤ) (b: ZM) : (x ^ a = x ^ (b.val: ℤ)) ↔ (a = b) := by
+    rw [zpow_eq_zpow_iff_modEq,
+        ← ZMod.intCast_eq_intCast_iff, order_x_eq_m,
+        ← ZMod.cast_eq_val, ZMod.intCast_zmod_cast b]
+  have right_inv : Function.RightInverse φ_inv φ := fun n =>
+    have ex := h_pow (φ_inv n)
+    (zpow_zmpow_eq ex.choose n).mp ex.choose_spec
+  let φ_Equiv : G ≃ Multiplicative ZM :=
+    Equiv.ofRightInverseOfCardLE (Nat.le_of_eq card_eq) φ φ_inv right_inv
+  have left_inv := φ_Equiv.left_inv
+  have map_mul' (g₁ g₂ : G) : φ (g₁ * g₂) = φ g₁ * φ g₂ :=
+    let a := φ g₁
+    let b := φ g₂
+    let ha := left_inv g₁
+    let hb := left_inv g₂
+    calc φ (g₁ * g₂) = φ (x ^ (a.val: ℤ) * x ^ (b.val: ℤ)) := ha ▸ hb ▸ rfl
+         _ = φ (x ^ ((a.val: ℤ) + (b.val: ℤ))) := congrArg φ (zpow_add _ _ _).symm
+         _ = φ (x ^ ((a * b).val : ℤ)) :=
+          -- yikes
+          have : x ^ ((a.val: ℤ) + (b.val: ℤ)) = x ^ ((a*b).val: ℤ) := by
+            unfold_let a b φ ZM
+            simp_all only [ZMod.natCast_val, Int.cast_add, ZMod.intCast_cast, ZMod.cast_intCast']
+            rfl
+          congrArg φ this
+         _ = a * b := right_inv _
+         _ = φ (x ^ (a.val: ℤ)) * φ (x ^ (b.val: ℤ)) := right_inv _ ▸ right_inv _ ▸ rfl
+         _ = φ g₁ * φ g₂ := ha ▸ hb ▸ rfl
+  MulEquiv.mk φ_Equiv map_mul'
+
+end FinitelyGeneratedGroups
 
 section OrbitStabilizerSylow
 open MulAction
