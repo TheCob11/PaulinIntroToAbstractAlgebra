341e059582541b94a1a6156f59cc3e15ddd94591
Cob <25969162+TheCob11@users.noreply.github.com>
2024-09-02 13:00:39 -0400

    fixed orbit_stabilizer for new mathlib ver and made it a little nicer

--- PaulinIntroToAbstractAlgebra/Chapter3/OrbitStabilizerSylow.lean
+++ PaulinIntroToAbstractAlgebra/Chapter3/OrbitStabilizerSylow.lean
@@ -2,30 +2,31 @@ import Mathlib
 open MulAction
 open Function (Injective Surjective Bijective)
 
+theorem mem_subgroup_iff_quotient_eq [Group G] {S: Subgroup G} {x y : G} :
+  x⁻¹*y ∈ S ↔ @Eq (G ⧸ S) ⟦x⟧ ⟦y⟧ :=
+  by rw [← QuotientGroup.leftRel_apply, ← @Quotient.eq']; rfl
+
 theorem orbit_stabilizer (G : Type u) {S : Type v} [Group G] [MulAction G S] (s: S) :
   Subgroup.index (stabilizer G s) = Nat.card (orbit G s) :=
 
-  let G_quo_stab := G ⧸ stabilizer G s;
-  let φ (xStab : G_quo_stab) : orbit G s :=
+  let stab_s := stabilizer G s
+  let φ (xStab : G⧸stab_s) : orbit G s :=
     ⟨xStab.exists_rep.choose • s, mem_orbit _ _⟩
   have φ_Bij : Bijective φ :=
     -- turns out defining these two functions allows for a lot of automatic proving
-    have smul_s_eq_iff_coset_eq {x: G} {y: G} : (x•s = y•s) ↔ (@Eq G_quo_stab ⟦x⟧ ⟦y⟧) := by
-      rw [smul_eq_iff_eq_inv_smul, Eq.comm, smul_smul]
-      rw [← mem_stabilizer_iff]
-      rw [← QuotientGroup.leftRel_apply, ← @Quotient.eq', @Quotient.mk'_eq_mk]
-    have rep_choose_spec (xStab: G_quo_stab) := xStab.exists_rep.choose_spec;
+    have smul_s_eq_iff_coset_eq {x y: G} : x•s = y•s ↔ ⟦x⟧ = ⟦y⟧ := calc
+      _ ↔ s = x⁻¹•y•s := smul_eq_iff_eq_inv_smul x
+      _ ↔ s = (x⁻¹ * y)•s := smul_smul x⁻¹ y s ▸ Iff.rfl
+      _ ↔ (x⁻¹ * y) ∈ stab_s := comm
+      _ ↔ ⟦x⟧ = ⟦y⟧ := mem_subgroup_iff_quotient_eq
+    have rep_choose_spec (xStab: G⧸stab_s) := xStab.exists_rep.choose_spec;
 
-    have φ_Surj : Surjective φ := by
-      rw [Surjective, Subtype.forall]
-      intro _ hs'
-      obtain ⟨_, hx⟩ := Set.mem_range.mpr hs'
+    have φ_Surj : Surjective φ := fun ⟨_, hs'⟩ ↦ by
+      have ⟨_, hx⟩ := Set.mem_range.mpr hs'
       subst hx
-      simp_all only [φ, Subtype.mk.injEq, exists_eq]
-
-    have φ_Inj : Injective φ := by
-      rw [Injective]
-      intros
       simp_all only [φ, Subtype.mk.injEq]
+      exact exists_eq
+
+    have φ_Inj : Injective φ := fun _ _ _ ↦ by simp_all only [φ, Subtype.mk.injEq]
     ⟨φ_Inj, φ_Surj⟩
   Nat.card_eq_of_bijective φ φ_Bij
